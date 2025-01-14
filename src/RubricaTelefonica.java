3329de8f2e8afd37de09eae6d290f5c0e42b93c6
Francimala05 <francimalafronte05@gmail.com>
2025-01-14 17:25:38 +0100

    Esercizio finito

--- src/RubricaTelefonica.java
+++ src/RubricaTelefonica.java
@@ -13,8 +13,9 @@ public class RubricaTelefonica {
             System.out.println("➡️Digita 0 per uscire dal programma");
             System.out.println("➡️Digita 1 per inserire un contatto");
             System.out.println("➡️Digita 2 per eliminare un contatto");
-            System.out.println("➡️Digita 3 per cercare un contatto");
-            System.out.println("➡️Digita 4 per mostrare tutti i contatti");
+            System.out.println("➡️Digita 3 per cercare un nome");
+            System.out.println("➡️Digita 4 per cercare un numero");
+            System.out.println("➡️Digita 5 per mostrare tutti i contatti");
             scelta = scanner.nextInt();
             switch (scelta) {
                 case 0:
@@ -22,18 +23,18 @@ public class RubricaTelefonica {
                     break;
                 case 1:
                     registraContatto(listaMap);
-                    System.out.println("Riepilogo contatto registrato: ");
-                    String nome = listaMap.keySet().iterator().next();
-                    String numero = listaMap.get(nome);
-                    System.out.println("Nome: " + nome + " Numero: " + numero);
+
            break;
                 case 2:
-                    System.out.println("Funzione ancora non funzionante");
+                    eliminaContatto(listaMap);
                     break;
                 case 3:
-                    System.out.println("Funzione ancora non funzionante");
+                    cercaContatto(listaMap);
                     break;
                 case 4:
+                    cercaNumero(listaMap);
+                    break;
+                case 5:
                     if (listaMap.isEmpty()) {
                         System.out.println("La rubrica è vuota.");
                     } else {
@@ -50,8 +51,6 @@ public class RubricaTelefonica {
         }
 
 
-
-
     }
     public static void registraContatto(Map<String,String> listaMap){
         Scanner scanner = new Scanner(System.in);
@@ -60,9 +59,48 @@ public class RubricaTelefonica {
         System.out.print("Inserisci il numero della persona: ");
         String numero = scanner.nextLine();
         listaMap.put(nome,numero);
+        System.out.println("Riepilogo contatto registrato: ");
+         nome = listaMap.keySet().iterator().next();
+         numero = listaMap.get(nome);
+        System.out.println("Nome: " + nome + " Numero: " + numero);
     }
 
+    public static void eliminaContatto(Map<String, String> listaMap) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci il nome del contatto da eliminare: ");
+        String nome = scanner.nextLine();
 
+        if (listaMap.containsKey(nome)) {
+            listaMap.remove(nome);
+            System.out.println("Contatto '" + nome + "' eliminato con successo!");
+        } else {
+            System.out.println("Il contatto con il nome '" + nome + "' non esiste nella rubrica");
+        }
+    }
+
+    public static void cercaContatto(Map<String, String> listaMap) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci il nome del contatto da cercare: ");
+        String nome = scanner.nextLine();
+        if (listaMap.containsKey(nome)) {
+            System.out.println("Ecco il contatto richiesto: " + nome + " - Numero: " + listaMap.get(nome));
+
+        } else {
+            System.out.println("Il contatto con il nome '" + nome + "' non esiste nella rubrica");
+        }
+    }
+
+    public static void cercaNumero(Map<String, String> listaMap) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci il numero del contatto da cercare: ");
+        String numero = scanner.nextLine();
+        if (listaMap.containsValue(numero)) {
+            System.out.println("Ecco il contatto richiesto: " + listaMap.get(numero) + " - Numero: " + numero);
+
+        } else {
+            System.out.println("Il contatto con il numero '" + numero + "' non esiste nella rubrica");
+        }
+    }
 
 
     }
